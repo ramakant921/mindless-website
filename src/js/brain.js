150e25e5605017e9c928e7ad6974beefb3bda263
zalos <satya123m4@gmail.com>
2025-09-27 19:08:04 +0530

    add: github widget is alive jesus

--- src/js/brain.js
+++ src/js/brain.js
@@ -12,16 +12,16 @@ fetch('http://localhost:3000/github/repos', {
     method: 'GET',
 })
 .then(res => res.json())
-.then(track => {
-    console.log(track);
+.then(repos => {
+    listRepos(repos);
 });
 
 fetch('http://localhost:3000/github/forks', {
     method: 'GET',
 })
 .then(res => res.json())
-.then(track => {
-    console.log(track);
+.then(forks => {
+    listRepos(forks);
 });
 
 fetch('http://localhost:3000/github/inbox', {
@@ -29,5 +29,48 @@ fetch('http://localhost:3000/github/inbox', {
 })
 .then(res => res.json())
 .then(notifications => {
-    console.log(notifications);
+    listInbox(notifications);
 });
+
+function listRepos(repos) {
+    repos.map(repo => {
+        // Grab github-repo section
+        const section = document.getElementById("github-repo");
+
+        // dynamically create div and add class and text
+        const block = document.createElement("div");
+        block.classList.add("blocks");
+        block.innerText = repo;
+
+        // append block to github-repo section
+        section.appendChild(block);
+    })
+}
+
+function listForks(repos) {
+    repos.map(repo => {
+        // Grab github-fork section
+        const section = document.getElementById("github-fork");
+
+        const block = document.createElement("div");
+        block.classList.add("blocks");
+        block.innerText = repo;
+
+        // append block to github-fork section
+        section.appendChild(block);
+    })
+}
+
+function listInbox(inbox) {
+    inbox.map(notification => {
+        // Grab github-inbox section
+        const section = document.getElementById("github-inbox");
+
+        const block = document.createElement("div");
+        block.classList.add("blocks");
+        block.innerText = notification;
+
+        // append block to github-inbox section
+        section.appendChild(block);
+    })
+}
