3574635e1f05a1c1163f1f735a1d423ea74d1060
Ramakant MIshra <ramakantm921@gmail.com>
2025-10-02 17:29:21 +0530

    todo changes

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
 const inputBox = document.getElementById("input-box");
+const addBtn = document.getElementById("add-btn");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!");
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = innerBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = "";
-    saveData();
+
+function addTask() {
+  if (inputBox.value === '') {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"; 
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === 'SPAN'){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === 'LI'){
-        e.target.parentElement.remove();
-        saveData();
-    }
+
+addBtn.addEventListener("click", addTask);
+
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
 }, false);
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
-function showTask(){
-listContainer.innerHTML = localStorage.getItem('data');
+
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
