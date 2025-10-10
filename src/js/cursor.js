c1bc1a7d25ab80eebffcf61eb7f33b030d5ed345
zalos <satya123m4@gmail.com>
2025-10-10 12:25:17 +0530

    fix: custom cursor lag

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -1,23 +1,24 @@
-const cursor = document.getElementById("cursor");
-console.log(cursor);
+let mouseX = 0, mouseY = 0;
+const cursor = document.getElementById('cursor');
 
+document.addEventListener('mousemove', e => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
 
-const moveCursor = (e) => {
     const elementUnderCursor = document.elementFromPoint(e.clientX, e.clientY);
-    
+
     // Get the cursor type 
     const cursorType = window.getComputedStyle(elementUnderCursor).cursor;
     cursor.style.display = "block";
     if(cursorType == "pointer") {
         cursor.style.display = "none";
     }
+});
 
-    console.log(e.target.style);
-    const mouseY = e.clientY;
-    const mouseX = e.clientX;
-
+function animate() {
     cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+    requestAnimationFrame(animate);
 }
 
-document.addEventListener("mousemove", moveCursor);
+animate();
 
