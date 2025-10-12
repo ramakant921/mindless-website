92893f76aa9d18a94b66cb7946715ec1d817de92
zalos <satya123m4@gmail.com>
2025-10-12 14:30:57 +0530

    settings & icon hover

--- src/js/brain.js
+++ src/js/brain.js
@@ -11,3 +11,21 @@ const githubInbox = document.getElementById("github-inbox");
 //     emailInbox.classList.add("active");
 // });
 
+// Play Audio On Hover
+const settigns = document.getElementById("settings-inner");
+const socialIcons = document.querySelectorAll(".social-icon");
+
+settigns.addEventListener("mouseenter", () => {
+    PlayAudio("../assets/audio/gear.mp3");
+});
+
+socialIcons.forEach((icon) => {
+    icon.addEventListener("mouseenter", () => {
+        PlayAudio("../assets/audio/icon-hover.wav");
+    });
+});
+
+function PlayAudio(filename) {
+    new Audio(filename).play();
+}
+
